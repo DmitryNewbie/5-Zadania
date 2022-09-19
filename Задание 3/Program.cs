ff12e53f83e04bf702887f47e9393032bedf78fd
Dmitry <zimzulka1@gmail.com>
2022-09-19 17:55:14 +0400

    first commit

---
+++ Задание 3/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] Number = new int[10];
+  for (int i = 0; i < Number.Length; i++ )
+  {
+    Number[i] = new Random().Next(1, 10);
+    Console.Write(Number[i] + " ");
+  }
+
+int maxNumber = Number[0];
+int minNumber = Number[0];
+
+  for (int i = 1; i < Number.Length; i++)
+  {
+    if (maxNumber < Number[i])
+    {
+      maxNumber = Number[i];
+    }
+        if (minNumber > Number[i])
+    {
+      minNumber = Number[i];
+    }
+  }
+
+  int decision = maxNumber - minNumber;
+Console.WriteLine();
+Console.WriteLine($"РАзница {decision}");
---
+++ задание 1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+Random(numbers);
+Print(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"Четных чисел {count}");
+
+void Random(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void Print(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
